02552caa58c37fa992090d76f1f5475cf49714ea
mayycf <may.fu@duke.edu>
2022-07-16 13:21:47 -0400

    adding icons

---
+++ rate-duke-app/src/icons/DukeLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './duke_wordmark_white.png'; 
+import './DukeLogo.css'
+
+function Duke() {
+  return <img class="duke-logo" src={logo} alt="Logo" />;
+}
+
+export default Duke;
--- rate-duke-app/src/pages/Home.jsx
+++ rate-duke-app/src/pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Duke from '../icons/DukeLogo';
 
 export default function Home() {
-    return(
-        <h1>hello you are in home page</h1>
+    return (
+        <Box 
+            sx={{ 
+                flexGrow: 1, 
+                backgroundColor: 'blue',
+                height: '100%',
+                display: 'block',
+            }}
+        >
+            <Grid container spacing={2}>
+                <Grid xs={6}>
+                    <Typography sx={{color: 'white'}}>
+                        Rate My
+                    </Typography>
+                </Grid>
+                <Grid xs={6}>
+                    <Duke />
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
+
